d84ad0fb5a206b5710b4fcbb509ea7511cf1372f
shigeyukey <shigge.3.21@gmail.com>
2024-09-25 05:47:31 +0900

    update

--- custom_by_shige/shige_pop/change_log.py
+++ custom_by_shige/shige_pop/change_log.py
@@ -2,6 +2,10 @@
 
 
 OLD_CHANGE_LOG = """
+[1] Enhanced 2024-09-23
+    - Enhanced tooltips.
+    - Added description to All column.
+
 [1] First Release 2024-09-19
     - Fixed problem with error on click
     - Added options window
